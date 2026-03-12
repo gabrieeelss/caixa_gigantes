4ded2005ae59b379b97b85a8e41dbee64b9ce245
Gabriel de Souza Simplício <gabrieeel.08@gmail.com>
2026-03-11 22:21:05 -0300

    login parcialmente funcionando

--- index.js
+++ index.js
@@ -1,4 +1,5 @@
 const cors = require('cors')
+const session = require("express-session")
 const express = require('express')
 const app = express()
 /* Indica que todas as requisições podem receber Body em JSON. A partir 
@@ -26,6 +27,55 @@ const conexao = mysql.createPool({
 })
 
 module.exports = conexao
+
+app.use(session({
+    secret: "caixa_gigantes",
+    resave: false,
+    saveUninitialized: true
+}))
+
+// LOGIN
+app.post("/login/", function (req, res) {
+    const login = req.body.login
+    const senha = req.body.senha
+
+    const sql = "SELECT * FROM usuarios WHERE login = ? AND senha = ?"
+    conexao.query(sql,[login,senha], function (erro, resultado) {
+        if (erro) {
+            res.send(erro)
+        } else {
+            if (resultado.length > 0) {
+
+                req.session.usuario = {
+                    id: resultado[0].id,
+                    login: resultado[0].login,
+                    nivel: resultado[0].nivel_acesso
+                }
+                res.sendStatus(200)
+            } else {
+                res.sendStatus(401)
+            }
+        }
+    })
+})
+
+// VERIFICAR LOGIN
+function verificarLogin(req,res, next){
+    if (req.session.usuario) {
+        next()
+    } else {
+        res.status(401).json({ erro: "Não autorizado"})
+    }
+}
+
+// PERMISSAO DE ADMIN
+function somenteAdmin(req,res,next){
+    if(req.session.usuario.nivel === "admin"){
+        next()
+    }else{
+        res.status(403).send("Acesso negado")
+    }
+}
 
 // CADASTRAR ASSOCIADO
 app.post("/cad-associado", function (req, res) {
---
+++ site/login-app.js
@@ -0,0 +1,30 @@
+function fnFazerLogin() {
+let formDados = {
+login: document.getElementById("login").value,
+senha: document.getElementById("senha").value
+}
+fetch('http://127.0.0.1:3000/login/', {
+method: 'POST',
+headers: {
+'Content-Type': 'application/json',
+},
+body: JSON.stringify(formDados),
+credentials: 'include'
+})
+.then(resposta => resposta.status)
+.then((status) => {
+    if(status == 200){
+        window.location.href = "caixa.html"
+    }else{
+        alert("Login inválido")
+    }
+
+})
+.catch(erro => console.log(erro.message))
+}
+
+let btn_login = document.getElementById("btn-login")
+
+btn_login.addEventListener("click", function () {
+    fnFazerLogin()
+})
